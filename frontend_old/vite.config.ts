83667ef08a4cd634a80f45b439ed8fadff6b4cfb
Mazitov, Dmitry <dmitry.mazitov@x5.ru>
2024-11-07 10:57:15 +0300

    add

--- frontend_old/vite.config.ts
+++ frontend_old/vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: { modules: { localsConvention: "camelCaseOnly" } },
   build: {
     rollupOptions: {
       input: {
